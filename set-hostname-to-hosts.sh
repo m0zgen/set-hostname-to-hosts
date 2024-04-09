c961afa6d7471554017eebb379932dc63fdd45d0
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-04-09 12:26:09 +0500

    + Check localhost exists and add if not

--- set-hostname-to-hosts.sh
+++ set-hostname-to-hosts.sh
@@ -8,14 +8,17 @@ hostname=$(cat /etc/hostname)
 # Get the first octet of the hostname
 first_octet=$(echo $hostname | cut -d'.' -f1)
 
+# Standard 127.0.0.1 localhost entry
+standard_entry="127.0.0.1\tlocalhost"
+
 # Create a new entry to add to the /etc/hosts file
 new_entry="127.0.1.1\t$hostname $first_octet"
 
 # Check if the entry already exists in the /etc/hosts file
 if ! grep -q "^127.0.0.1" /etc/hosts; then
-    echo -e "127.0.0.1	localhost" | tee /etc/hosts
+    # Add as first line the standard entry to the /etc/hosts file
+    sed -i "1i 127.0.0.1\tlocalhost" /etc/hosts
 fi
-
 
 # Check if the entry already exists in the /etc/hosts file
 if grep -q "^127.0.1.1" /etc/hosts; then
@@ -23,6 +26,7 @@ if grep -q "^127.0.1.1" /etc/hosts; then
     sed -i "/^127.0.1.1/d" /etc/hosts
 fi
 
+# Secondary check if the entry already exists
 # Check if the entry already exists in the /etc/hosts file
 if ! grep -q "^127.0.1.1" /etc/hosts; then
     # Add the new entry to the /etc/hosts file
