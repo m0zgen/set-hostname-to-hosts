450abb883bb299b98bd26fd9bab9e3a85afafff4
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-04-09 12:16:21 +0500

    + First release

---
+++ set-hostname-to-hosts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Created by Yevgeniy Goncharov, https://lab.sys-adm.in
+# Update the /etc/hosts file to include the current hostname
+
+# Get the hostname from the /etc/hostname file
+hostname=$(cat /etc/hostname)
+
+# Get the first octet of the hostname
+first_octet=$(echo $hostname | cut -d'.' -f1)
+
+# Create a new entry to add to the /etc/hosts file
+new_entry="127.0.1.1\t$hostname $first_octet"
+
+# Check if the entry already exists in the /etc/hosts file
+if grep -q "^127.0.1.1" /etc/hosts; then
+    # Если строка существует, удаляем ее
+    sed -i "/^127.0.1.1/d" /etc/hosts
+fi
+
+# Check if the entry already exists in the /etc/hosts file
+if ! grep -q "^127.0.1.1" /etc/hosts; then
+    # Add the new entry to the /etc/hosts file
+    sed -i "/^127.0.0.1/a $new_entry" /etc/hosts
+else
+    # Add the new entry to the /etc/hosts file
+    echo -e "127.0.0.1	localhost" | tee /etc/hosts
+    echo -e $new_entry | tee -a /etc/hosts
+fi
