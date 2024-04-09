220a002c28477fa34c4cf3ccd8d94e06d5a3bea9
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-04-09 12:30:56 +0500

    + + Add script sections

---
+++ README.md
@@ -0,0 +1,10 @@
+# Set / Update hostname in hosts
+
+Script to set or update hostname in hosts file.
+
+## Usage
+
+```bash
+$ ./set-hostname.sh
+```
+
--- set-hostname-to-hosts.sh
+++ set-hostname-to-hosts.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Created by Yevgeniy Goncharov, https://lab.sys-adm.in
 # Update the /etc/hosts file to include the current hostname
+# Sys env / paths / etc
+# -------------------------------------------------------------------------------------------\
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd); cd $SCRIPT_PATH
+
+# Variables
+# -------------------------------------------------------------------------------------------\
 
 # Get the hostname from the /etc/hostname file
 hostname=$(cat /etc/hostname)
@@ -13,6 +20,9 @@ standard_entry="127.0.0.1\tlocalhost"
 
 # Create a new entry to add to the /etc/hosts file
 new_entry="127.0.1.1\t$hostname $first_octet"
+
+# Main
+# -------------------------------------------------------------------------------------------\
 
 # Check if the entry already exists in the /etc/hosts file
 if ! grep -q "^127.0.0.1" /etc/hosts; then
